d3558c3ffba8977bbeef247ff6c807954bcd3a3c
Andrey Zverev <a.zverev@severnaya.ru>
2023-12-25 00:23:34 +0300

    востановил все зависимости после сбоя

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 200, // Limit each IP to 100 requests
+  max: 200, // Limit each IP to 200 requests
 });
 
 const app = express();
